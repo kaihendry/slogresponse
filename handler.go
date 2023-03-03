1e8327da21fc70b6650c188692e1b23f637685a1
Kai Hendry <hendry@iki.fi>
2023-03-04 04:35:16 +0800

    feat: add duration

--- handler.go
+++ handler.go
@@ -2,17 +2,22 @@ package slogresponse
 
 import (
 	"net/http"
+	"time"
 
 	"golang.org/x/exp/slog"
 )
 
 func New(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		slog.Info("slogresponse",
-			"req_method", r.Method,
-			"req_ip", r.RemoteAddr,
-			"req_path", r.RequestURI,
-		)
+		start := time.Now()
+		defer func() {
+			slog.Info("slogresponse",
+				"req_method", r.Method,
+				"req_ip", r.RemoteAddr,
+				"req_path", r.RequestURI,
+				slog.Duration("duration", time.Since(start)),
+			)
+		}()
 		h.ServeHTTP(w, r)
 	})
 }
